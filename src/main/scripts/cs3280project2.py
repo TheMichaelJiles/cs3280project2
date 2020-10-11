64dc8ecd1e5a2e73b6f327df321a0e10292b0c03
TheMichaelJiles <sjiles1@my.westga.edu>
2020-10-11 12:57:45 -0400

    completed IPV4 functionality, no error checking

--- src/main/scripts/cs3280project2.py
+++ src/main/scripts/cs3280project2.py
@@ -7,28 +7,19 @@ import masker
 def main():
     ip = sys.argv[1]
     mask = sys.argv[2]
-    mask = convertN_NotationToOctet(mask)
-    print("Result: " + masker.applySubnetMask(ip, mask))
 
-def convertN_NotationToOctet(mask):
-    mask = mask.replace('/', '')
-    mask_int = int(mask)
-    bin_ip = ''
-    ip_list = []
-    for index in range(32):
-        if index < mask_int:
-            bin_ip += '1'
+    if validation.isValidBitNumberNotationNetmask(mask):
+        mask = masker.convertN_NotationToOctet(mask)
+
+    if (validation.isValidIPV4Address(ip)):
+        if validation.isValidOctectNetmask(mask):
+            print("Result: " + masker.applySubnetMask(ip, mask))
         else:
-            bin_ip += '0'
-
-        if (index + 1) % 8 == 0:
-            ip_list.append(int(bin_ip, 2))
-            bin_ip = ''
-
-    result = ''
-    for number in ip_list:
-        result = result + str(number) + '.'
-    return result[:-1]
+            print("ERROR: MASK INVALID")
+            #throw mask invalid error
+    else:
+        print("ERROR: IP INVALID")
+        #throw ip invalid error
 
 if __name__ == '__main__':
     main()
--- src/main/scripts/masker.py
+++ src/main/scripts/masker.py
@@ -14,3 +14,22 @@ def applySubnetMask(ip, mask):
 
     return result[:-1]
 
+def convertN_NotationToOctet(mask):
+    mask = mask.replace('/', '')
+    mask_int = int(mask)
+    bin_ip = ''
+    ip_list = []
+    for index in range(32):
+        if index < mask_int:
+            bin_ip += '1'
+        else:
+            bin_ip += '0'
+
+        if (index + 1) % 8 == 0:
+            ip_list.append(int(bin_ip, 2))
+            bin_ip = ''
+
+    result = ''
+    for number in ip_list:
+        result = result + str(number) + '.'
+    return result[:-1]
